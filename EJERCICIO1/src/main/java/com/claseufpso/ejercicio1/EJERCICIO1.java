fceed04e32bf1641a1b41d808f446c8b9de333fc
Luiggyblanco24 <24luiggy@gmail.com>
2024-02-29 15:21:06 -0500

    creacion de codigo cuca

--- EJERCICIO1/src/main/java/com/claseufpso/ejercicio1/EJERCICIO1.java
+++ EJERCICIO1/src/main/java/com/claseufpso/ejercicio1/EJERCICIO1.java
@@ -1,16 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.claseufpso.ejercicio1;
 
-/**
- *
- * @author SCIS2PC21
- */
 public class EJERCICIO1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Characther homero = new Characther("homero",30 ,'m' ,"a la grande le puse cuca");
+        homero.saySomething();
+        
+        Characther bart = new Characther("bartolomeo",10 ,'m' ,"ay caramba");
+        bart.saySomething();
+        
+    }
+    
+    static class Characther{
+        String name;
+        int age;
+        char gender;
+        String phrase;
+
+        public Characther(String name, int age, char gender, String phrase) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.phrase=phrase;
+        }
+        
+        void saySomething(){
+            System.out.println(this.phrase );
+        
+        }
     }
 }
